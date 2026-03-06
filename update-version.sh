ee44563c0d9f2e5fb4abd2e22029a33ad4620003
jyz0501 <jyz_0501@live.cn>
2026-03-06 10:31:37 +0800

    feat: 版本号改为 v2 前缀

--- update-version.sh
+++ update-version.sh
@@ -4,7 +4,7 @@
 COMMIT_COUNT=$(git rev-list --count HEAD)
 
 # 生成版本号（基于提交次数）
-VERSION="v1.$COMMIT_COUNT.0"
+VERSION="v2.$COMMIT_COUNT.0"
 
 # 更新 index.html 中的版本号
 sed -i.bak "s|<span id=\"app-version\"[^>]*>.*</span>|<span id=\"app-version\" style=\"font-size: 12px; color: #999;\">$VERSION</span>|" index.html
